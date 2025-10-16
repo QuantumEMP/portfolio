4d0dbff242a3c909889737aef4b7af91e6a480b4
jurosecpt024 <jurosecpt024@student.wethinkcode.co.za>
2025-10-16 17:24:31 +0200

    Start

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+            <Route index element={}/>
+            <Route />
+        </Routes>
+      </BrowserRouter>
+    </>
+  )
+}
+
+export default App
