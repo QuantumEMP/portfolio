514df3c1af730cec6e4f6242bf50eb5f9b98b7d1
QuantumEMP <jude1rose@outlook.com>
2026-08-12 13:50:00 +0000

    Adding skills

--- app/components/Skills.vue
+++ app/components/Skills.vue
@@ -1,4 +1,106 @@
-<script setup lang="ts" />
+<script setup lang="ts">
+
+const skills   = [
+  // {"Category": "",
+  //  "Name": "",
+  //  "Example": ""
+  // }
+  {"Category":"FrontEnd",
+   "Name": "VueJS",
+   "Example": "friendship-census.jude-rose.com",
+   "Frameworks": [{
+    "Name": "NuxtJS/NuxtUI",
+    "Example": "friendship-census.jude-rose.com"
+   }, {
+    "Name": "NextJS"
+   }]
+  },
+  {"Category":"FrontEnd",
+    "Name": "ReactJS",
+    "Example": "portfolio.jude-rose.com"
+  },
+  {"Category": "FrontEnd",
+   "Name": "Wordpress",
+   "Example": "jude-rose.com/blog",
+   "Builders": [{
+    "Name": "Elementor",
+    "Example": "gurliepurlie.shop"
+   }, {
+    "Name": "WooStack",
+    "Example": "gurliepurlie.shop"
+   }]
+  },
+  {"Category": "FrontEnd",
+    "Name": "TailwindCSS",
+    "Example": "/"
+  },
+  {"Category": "FrontEnd",
+    "Name": "Storybook",
+    "Example": ""
+  },
+  {"Category": "FrontEnd",
+    "Name": "Vite",
+    "Example": "portfolio.jude-rose.com"
+  },
+  {"Category": "FrontEnd",
+    "Name": "HTML",
+    "Example": ""
+  },
+  {"Category": "FrontEnd",
+    "Name": "CSS",
+    "Example": ""
+  },
+  {"Category": "BackEnd",
+    "Name": "NodeJS",
+    "Example": "friendship-census.jude-rose.com"
+  },
+  {"Category": "BackEnd",
+    "Name": "JavaScript",
+    "Example": "portfolio.jude-rose.com"
+  },
+  {"Category": "BackEnd",
+    "Name": "Java",
+    "Example": "https://github.com/QuantumEMP/sudoku"
+  },
+  {"Category": "BackEnd",
+    "Name": "Python",
+    "Example": "https://github.com/QuantumEMP/UNO"
+  },
+  {"Category": "BackEnd",
+    "Name": "FastAPI",
+    "Example": ""
+  },
+  {"Category": "BackEnd",
+    "Name": "SpringBoot",
+    "Example": ""
+  },
+  {"Category": "BackEnd",
+    "Name": "PHP",
+    "Example": "gurliepurlie.shop"
+  },
+  {"Category": "BackEnd",
+    "Name": "Firebase",
+    "Example": ""
+  },
+  {"Category": "BackEnd",
+    "Name": "ExpressJS",
+    "Example": ""
+  },
+  {"Category": "Data Engineering",
+    "Name": "SQL",
+    "Example": "friendship-census.jude-rose.com"
+  }
+]
+const misc = [
+  // {"Category": "Miscellaneous",
+  //   "Name": "",
+  //   "Years": ""
+  // }
+]
+
+
+
+</script>
 
 <template>
   <h3 class="sk-head"> ♦ Skills</h3>
